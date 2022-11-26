6440b669aac47775ae0a4c6f68feeaab0d8ef775
Greg Cotten <greg@gregcotten.com>
2022-11-25 17:32:33 -0800

    remove human interfaces

--- Sources/SwiftCOM/Interfaces/Human/ID3D12Pageable.swift
+++
@@ -1,8 +0,0 @@
-// Copyright 2020 Saleem Abdulrasool <compnerd@compnerd.org>. All Rights Reserved.
-// SPDX-License-Identifier: BSD-3-Clause
-
-import WinSDK
-
-public class ID3D12Pageable: ID3D12DeviceChild {
-  override public class var IID: IID { IID_ID3D12Pageable }
-}
--- Sources/SwiftCOM/Interfaces/Human/ID3D12StateObject.swift
+++
@@ -1,8 +0,0 @@
-// Copyright 2020 Saleem Abdulrasool <compnerd@compnerd.org>. All Rights Reserved.
-// SPDX-License-Identifier: BSD-3-Clause
-
-import WinSDK
-
-public class ID3D12StateObject: ID3D12Pageable {
-  override public class var IID: IID { IID_ID3D12StateObject }
-}
